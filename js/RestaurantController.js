850ffd28f015f0faf3be2f178df2b2ccde294865
Kathryn Carr and Arsi <kathrynelizabethcarr@gmail.com>
2015-02-23 15:28:40 -0800

    Add checkboxes for food type.

--- js/RestaurantController.js
+++ js/RestaurantController.js
@@ -1,3 +1,29 @@
 restaurantList.controller('RestaurantCtrl', function RestaurantCtrl($scope){
   $scope.restaurants = [];
+  $scope.restaurants.types = [];
+
+  $scope.addRestaurant = function(){
+
+    $scope.restaurants.push({ name: $scope.restaurantName,
+                          location: $scope.restaurantQuadrant,
+                          foodType: $scope.restaurantType,
+                             price: $scope.restaurantPrice
+
+    });
+
+    if($scope.chinese){
+      $scope.restaurants.types.push("Chinese");
+    }
+
+    if($scope.japanese){
+      $scope.restaurants.types.push("Japanese");
+    }
+
+
+    $scope.restaurantName = null;
+    $scope.restaurantQuadrant = null;
+    $scope.restaurantType = null;
+    $scope.restaurantPrice = null;
+  };
+
 });
