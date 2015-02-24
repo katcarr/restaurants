595cc3cbddee02c6515044241eddc772ee8b9d71
Kathryn Carr and Arsi <kathrynelizabethcarr@gmail.com>
2015-02-23 16:29:45 -0800

    Add checkBoxController. Only true values are saved to an array.

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 var restaurantList = angular.module('restaurantList', []);
+
+
+restaurantList.controller("checkBoxController", function($scope){
+  $scope.types = [{name: "Chinese"}, {name: "Japanese"}];
+  $scope.selects=[];
+
+  $scope.toggleSelection = function toggleSelction(types){
+    var idx = $scope.selects.indexOf(types);
+
+    if(idx > -1){
+      $scope.selects.splice(idx, 1);
+      console.log($scope.selects);
+    } else {
+      $scope.selects.push(types);
+      console.log($scope.selects);
+    }
+
+  };
+
+});
--- js/RestaurantController.js
+++ js/RestaurantController.js
@@ -1,29 +1,35 @@
-restaurantList.controller('RestaurantCtrl', function RestaurantCtrl($scope){
-  $scope.restaurants = [];
-  $scope.restaurants.types = [];
+// restaurantList.controller('RestaurantCtrl', function RestaurantCtrl($scope){
+//
+//
+//   $scope.restaurants = {};
+//
+//   console.log($scope.restaurants);
 
-  $scope.addRestaurant = function(){
+  // $scope.restaurants = [];
+  // $scope.restaurants.types = [];
+  //
+  // $scope.addRestaurant = function(){
+  //
+  //   $scope.restaurants.push({ name: $scope.restaurantName,
+  //                         location: $scope.restaurantQuadrant,
+  //                         foodType: $scope.restaurantType,
+  //                            price: $scope.restaurantPrice
+  //
+  //   });
+  //
+  //   if($scope.chinese){
+  //     $scope.restaurants.types.push("Chinese");
+  //   }
+  //
+  //   if($scope.japanese){
+  //     $scope.restaurants.types.push("Japanese");
+  //   }
+  //
+  //
+  //   $scope.restaurantName = null;
+  //   $scope.restaurantQuadrant = null;
+  //   $scope.restaurantType = null;
+  //   $scope.restaurantPrice = null;
+  // };
 
-    $scope.restaurants.push({ name: $scope.restaurantName,
-                          location: $scope.restaurantQuadrant,
-                          foodType: $scope.restaurantType,
-                             price: $scope.restaurantPrice
-
-    });
-
-    if($scope.chinese){
-      $scope.restaurants.types.push("Chinese");
-    }
-
-    if($scope.japanese){
-      $scope.restaurants.types.push("Japanese");
-    }
-
-
-    $scope.restaurantName = null;
-    $scope.restaurantQuadrant = null;
-    $scope.restaurantType = null;
-    $scope.restaurantPrice = null;
-  };
-
-});
+// });
